816d066b40fdd83cd717e2fae2554ba117b2c05f
Reinaldo Assis <randroi75@gmail.com>
2022-06-07 21:40:35 -0300

    feat: init huffman

--- src/huffman/helpers.c
+++ src/huffman/helpers.c
@@ -1,7 +1,29 @@
-#include "helpers.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
+#include "helpers.h"
 
 void get_bytes_frequency()
 {
+    FILE *f = fopen("./test.txt","r");
     
+    if(f != NULL)
+    {
+        unsigned char buffer[4096];
+        size_t sz;
+        while((sz = fread(buffer, 1, sizeof(buffer), f)) > 0)
+        {
+            for(int i = 0; i < sz; i++)
+                //printf("%d", buffer[i]);
+                //or use "%02X" instead, for printing the byte values:
+                printf("%02X ", buffer[i]);
+        }
+        printf("\n");
+
+    }else
+    {
+        printf("Algo de errado aconteceu ao tentar abrir o arquivo :(\n");
+    }
+
 }
--- src/huffman/helpers.h
+++ src/huffman/helpers.h
@@ -1,2 +1,33 @@
 
+#define MAX_BYTES_TO_READ 100000000 //100 mb
+#define MAX_HEAP_SIZE 10000000
+
 void get_bytes_frequency();
+
+//======================================
+
+//PRIORITY QUEUE
+
+typedef struct binaryheapQueue
+{
+    int size;
+    char data[MAX_HEAP_SIZE];
+} binaryheapQueue;
+
+binaryheapQueue *newHeapQueue();
+
+void enqueue_heap(binaryheapQueue *heap, char item, int *comparacoes);
+
+char dequeue_heap(binaryheapQueue *heap);
+
+int get_parent_index(binaryheapQueue *heap, int i);
+
+int get_left_index(binaryheapQueue *heap, int i);
+
+int get_right_index(binaryheapQueue *heap, int i);
+
+void min_heapify(binaryheapQueue *heap, int i);
+
+char item_of(binaryheapQueue *heap, int i);
+
+void runQueueBenchmarking();
