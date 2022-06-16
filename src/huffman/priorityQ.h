d811ee7982852198b4836ce2fa8dd2b98ce07603
Reinaldo Assis <randroi75@gmail.com>
2022-06-16 19:41:45 -0300

    add: comments

--- src/huffman/priorityQ.h
+++ src/huffman/priorityQ.h
@@ -15,39 +15,58 @@ typedef struct hufftree_node
 
 //PRIORITY QUEUE
 
-// typedef struct heapq_node
-// {
-//     long long int priority;
-//     unsigned char value;
-
-// } heapq_node;
-
 typedef struct huffheapQueue
 {
     int size;
     hufftree_node *items[MAX_HUFF_Q];
 } huffheapQueue;
 
+//@return priority queue
 huffheapQueue *newHuffQueue();
 
+//@param value: byte value
+//@param priority: byte's frequency
+//@return huffmantree_node
 hufftree_node* newHeapNode(char value, int priority);
 
+//@param heap: priority queue
+//@param value: byte' value
+//@param priority: byte's frequency
 void huff_enqueue(huffheapQueue *heap, char value, int priority);
 
+//@param heap: priority queue
+//@param node: huffman tree node to be enqueued
 void huff_enqueue_from_node(huffheapQueue *heap, hufftree_node *node);
 
+//@param heap: priority queue
+//@return dequeued node
 hufftree_node* huff_dequeue(huffheapQueue *heap);
 
+//@param heap: priority queue
+//@param i: current index
+//@return parent index
 int huff_get_parent_index(huffheapQueue *heap, int i);
 
+//@param heap: priority queue
+//@param i: current index
+//@return left index
 int huff_get_left_index(huffheapQueue *heap, int i);
 
+//@param heap: priority queue
+//@param i: current index
+//@return right index
 int huff_get_right_index(huffheapQueue *heap, int i);
 
+//@param heap: priority queue
+//@param i: current index
 void min_heapify_huff(huffheapQueue *heap, int i);
 
+//@param heap: priority queue
+//@param i: current index
+//@return hufftree_node from given index
 hufftree_node* huff_item_of(huffheapQueue *heap, int i);
 
+//swaps huffmantree nodes
 void swap(hufftree_node **x, hufftree_node **y);
 
 #endif
