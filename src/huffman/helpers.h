68de054a24417023021c48c2afe7b814cc626aa8
Reinaldo Assis <randroi75@gmail.com>
2022-06-16 19:15:07 -0300

    add: comments

--- src/huffman/helpers.h
+++ src/huffman/helpers.h
@@ -12,11 +12,13 @@
 // @param f: file to count bytes from
 void get_bytes_frequency(long long int *frequency, FILE *f);
 
-//@param f: file to be compressed
+//@param input: file to be compressed
 //@param path: output path
-void *compress(FILE *f, char *path);
+void *compress(FILE *input, char *path);
 
-void *decompress(FILE *f, FILE *output);
+//@param input: file to be decompressed
+//@param output: output file stream
+void *decompress(FILE *input, FILE *output);
 
 //======================================
 
@@ -35,14 +37,27 @@ typedef struct hashtable
     hashnode *table[256];
 } hashtable;
 
+//@param h: hashtable to print
 void print_hashtable(hashtable *h);
 
+//@param h: hashtable
+//@param byte: key to the hashtable
+//@param frequency: frequency of the byte
+//@param code: huffman code to be assigned
+//@param level: depth in the tree
 void put_hashtable(hashtable *h, unsigned char byte, long long int frequency, unsigned int code, int level);
 
+//@return new hashtable
 hashtable *newhashtable();
 
+//@return new hashnode
 hashnode *newhashnode();
 
+/*
+@param tb: hashtable
+@param key: byte/key
+@return hashnode
+*/
 hashnode* get_hashtable(hashtable *tb, unsigned char key);
 
 #endif
